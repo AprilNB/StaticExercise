a733daa58738c642bce9a8f95f7a59c4983e543f
April Bullard <aprilnb86@gmail.com>
2021-06-22 04:37:51 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FarenheitToCelsius(68);
+            var farenheit = TempConverter.CelciusToFarenheit(20);
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"farenheit {farenheit}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius(double farenheit)
+        {
+            var result = (farenheit - 32) / 1.8;
+
+            return result;
+        }
+
+        public static double CelciusToFarenheit(double celcius)
+        {
+            return (celcius * 9) / 5 + 32;
+        }
+    }
+
+}
